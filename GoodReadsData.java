87cbceabe0eb3dcd6285f181a5c49f614a89030d
crhm <y33059858+crhm@users.noreply.github.com>
2017-09-13 12:29:16 +0100

    Made the user input for the csv filename alittle more fool proof and flexible, by introducing a FileNotFoundException catch, and making it display an error message, but without asking for input again, aka the program pretty much just stops after that. This was per a SMALL todo. Also added some comments here and there

--- GoodReadsData.java
+++ GoodReadsData.java
@@ -5,7 +5,6 @@
 // TODO implement try/ catch blocks and error messages
 
 // SMALL
-// TODO make sure user input for file name is user-error proof
 
 /* The code if made to work with a raw, unmodified csv as it is given directly by Goodreads,
  * from the name to everything else, so no changes are needed on that end if the file is to be
@@ -17,6 +16,7 @@ import java.io.FileReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.HashMap;
+import java.io.FileNotFoundException;
 
 public class GoodReadsData { 
 
@@ -158,113 +158,133 @@ public class GoodReadsData {
 
 		try {
 			br1 = new BufferedReader(new InputStreamReader(System.in));
-			System.out.println("Please input name of csv file to be imported (without extension or path):");
-			String fileName = br1.readLine();
-			br2 = new BufferedReader(new FileReader("src/" + fileName + ".csv"));
-			StringBuilder sb = new StringBuilder();
-			while ((line = br2.readLine()) != null){
-				sb.append(line);
-				sb.append(System.lineSeparator());
-			}
-			String todo1 = sb.toString();
-			// Places each line as an element in an array of Strings
-			String[] todo2 = todo1.split("\n");
-
-			int counter = 0;
-
-			for (String el : todo2 ){
-				if (counter!=0){ // to avoid the first line of the header of the csv
-					
-					// Splits the line into its elements and places them in order in an array of Strings
-					// and makes sure that the comas stay within the ""
-					String[] temp = el.split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)", -1); 
-					
-					// Creates an empty 19-place string array to hold the 19 different informations needed
-					String[] book = new String[19];
-
-					// Takes each element in the temp and places it in the according position in book
-					// Stops after the 19 first elements, discarding the rest since it is useless info
-					// Like notes or number of copies owned
-					int count = 0;
-					for (String e : temp){
-						if(count<19){
-							book[count] = e.trim();
-							count++;
-						}
-					}
-					
-					// Uses values in book as the parameters of the relevant class instances,
-					// initialising doubles at -1 and strings as empty, 
-					// and then checking that the values exist and that they are not empty,
-					// checking the isbn is the right length,
-					// and creating the relevant class instance if they don't already exist
-					
-					long goodreadsID = Long.parseLong(book[0]);
-					String title = book[1].replace("\"", "");
-					String[] namesAuthor = book[3].split(", ");
-					String lastName = namesAuthor[0].replace("\"", "");
-					String firstName = namesAuthor[1].replace("\"", "");
-					
-					// To remove the doubling of names when authors go by a single name, like Colette
-					if (lastName.equals(firstName)) {
-						firstName = "";
-					}
-					
-					if (!getListAuthors().containsKey(lastName)){
-						listAuthorsAdd(new Author(firstName, lastName, 0 ));
-					}
-					
-					String isbn = "";
-					if (book[5]!= null && book[5].length()==17){
-						//isbn = book[5].substring(4, 14);
-						isbn = book[5].replaceAll("\"", "").replaceAll("=", "");
-
-					}
-					
-					double myRating = -1;
-					if (book[7]!= null && book[7].length() >0){
-						myRating = Double.parseDouble(book[7]);
-					}
-					
-					double avRating = -1;
-					if (book[8]!= null && book[8].length() >0){
-						avRating = Double.parseDouble(book[8]);
-					}
-					
-					double numPages = -1;
-					if (book[11]!= null && book[11].length() >0){
-						numPages = Double.parseDouble(book[11]); 
-					}
-					
-					String year = "";
-					if (book[13]!= null && book[13].length() >0){
-						year = book[13];
-					}
-					
-					String dateRead = "";
-					if (book[14]!= null && book[14].length() >0){
-						dateRead = book[14];
-					}
-					
-					String dateAdded = book[15];
-
-					if (!getListShelves().containsKey(book[18])){
-						listShelvesAdd(new Shelf(book[18]));
-					}
-
-					// Creates the book with its parameters
-					Book livre = new Book(title, getListAuthors().get(lastName), isbn, goodreadsID, 
-							numPages, year, avRating, myRating, dateRead, dateAdded, 
-							getListShelves().get(book[18]));
-					
-					listBooksAdd(livre);
-					
-					// Adds book to its shelf
-					livre.getShelf().addBook(livre);
-					// Adds book to its author
-					livre.getAuthor().addBook(livre);
+			System.out.println("Please input name of csv file to be imported (without path) (Please ensure that "
+					+ "the file is in the \"src\" folder):");
+			try {
+				String fileName = br1.readLine();
+				
+				// Ensures this works whether the user inputs the extension or not
+				if (fileName.endsWith(".csv")) {
+					br2 = new BufferedReader(new FileReader("src/" + fileName));
+				} else {
+					br2 = new BufferedReader(new FileReader("src/" + fileName + ".csv")); 
 				}
-				counter++;
+				
+				// Builds a string from the data in the csv file being read, where each csv line
+				// are on a separate line
+				StringBuilder sb = new StringBuilder();
+				while ((line = br2.readLine()) != null){
+					sb.append(line);
+					sb.append(System.lineSeparator());
+				}
+				String todo1 = sb.toString();
+				
+				// Places each line as an element in an array of Strings
+				String[] todo2 = todo1.split("\n");
+
+				int counter = 0;
+
+				for (String el : todo2 ){
+					if (counter!=0){ // to avoid the first line of the header of the csv, aka labels
+						
+						// Splits the line into its elements and places them in order in an array of Strings
+						// and makes sure that the comas stay within the ""
+						String[] temp = el.split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)", -1); 
+						
+						// Creates an empty 19-place string array to hold the 19 different informations needed
+						String[] book = new String[19];
+
+						// Takes each element in the temp and places it in the according position in book
+						// Stops after the 19 first elements, discarding the rest since it is useless info
+						// Like notes or number of copies owned
+						int count = 0;
+						for (String e : temp){
+							if(count<19){
+								book[count] = e.trim();
+								count++;
+							}
+						}
+						
+						// Uses values in book as the parameters of the relevant class instances,
+						// initialising doubles at -1 and strings as empty, 
+						// and then checking that the values exist and that they are not empty,
+						// checking the isbn is the right length,
+						// and creating the relevant class instance if they don't already exist
+						
+						long goodreadsID = Long.parseLong(book[0]);
+						
+						String title = book[1].replace("\"", "");
+						
+						// Takes the 4th csv column entry and splits it around the commma to obtain first
+						// and last name of the author
+						String[] namesAuthor = book[3].split(", ");
+						String lastName = namesAuthor[0].replace("\"", "");
+						String firstName = namesAuthor[1].replace("\"", "");
+						
+						// To remove the doubling of names when authors go by a single name, like Colette
+						if (lastName.equals(firstName)) {
+							firstName = "";
+						}
+						
+						if (!getListAuthors().containsKey(lastName)){
+							listAuthorsAdd(new Author(firstName, lastName, 0 ));
+						}
+						
+						String isbn = "";
+						if (book[5]!= null && book[5].length()==17){
+							//isbn = book[5].substring(4, 14); // What is this? What was it for?
+							isbn = book[5].replaceAll("\"", "").replaceAll("=", "");
+						}
+						
+						double myRating = -1;
+						if (book[7]!= null && book[7].length() >0){
+							myRating = Double.parseDouble(book[7]);
+						}
+						
+						double avRating = -1;
+						if (book[8]!= null && book[8].length() >0){
+							avRating = Double.parseDouble(book[8]);
+						}
+						
+						double numPages = -1;
+						if (book[11]!= null && book[11].length() >0){
+							numPages = Double.parseDouble(book[11]); 
+						}
+						
+						String year = "";
+						if (book[13]!= null && book[13].length() >0){
+							year = book[13];
+						}
+						
+						String dateRead = "";
+						if (book[14]!= null && book[14].length() >0){
+							dateRead = book[14];
+						}
+						
+						String dateAdded = book[15];
+
+						if (!getListShelves().containsKey(book[18])){
+							listShelvesAdd(new Shelf(book[18]));
+						}
+
+						// Creates the book with its parameters
+						Book livre = new Book(title, getListAuthors().get(lastName), isbn, goodreadsID, 
+								numPages, year, avRating, myRating, dateRead, dateAdded, 
+								getListShelves().get(book[18]));
+						
+						listBooksAdd(livre);
+						
+						// Adds book to its shelf
+						livre.getShelf().addBook(livre);
+						// Adds book to its author
+						livre.getAuthor().addBook(livre);
+					}
+					counter++;
+				}
+			} catch (FileNotFoundException e) {
+				System.out.println("Sorry, no such file was found in folder \"src\".\nPlease make sure that "
+						+ "the name was typed correctly (no path, no typo),\n"
+						+ "and that the file is in the \"src\" folder.\n");
 			}
 
 		} catch (Exception e){
