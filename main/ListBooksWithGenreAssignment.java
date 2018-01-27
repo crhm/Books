56c61e9b805adca97b78f052f9aa50eec34debce
crhm <33059858+crhm@users.noreply.github.com>
2018-01-27 10:34:10 +0000

    Changed Genre's class variable listOfBooks to a specially created subclass of ListBooks, ListBooksWithGenreAssignment, which keeps an extra HashMap on top of the ListBooks one to keep track of how many users assigned each book in the list to the Genre that will hold the ListBooksWithGenreAssignment.

---
+++ main/ListBooksWithGenreAssignment.java
@@ -0,0 +1,57 @@
+package main;
+
+import java.util.HashMap;
+
+/** Class that holds both the list of books of a genre, and the number of users who 
+ * assigned each book to the genre, in two separate class variables that are distinct HashMaps.
+ * <br>The traditional list of books is the same as for a standard ListBooks.
+ * <br>The extra list is 'listWithGenreAssignment' (method names follow this format), 
+ * which is a HashMap of Integers where their key is a Book; the integer represents 
+ * the number of users who assigned the book to the genre.
+ * <br>!!!Please note!!!: There is NO synchronisation of the two HashMaps; adding a book to one does not
+ * add it to the other. This must be done separately by calling the other add method.
+ * @author crhm
+ *
+ */
+public class ListBooksWithGenreAssignment extends ListBooks {
+	
+	private HashMap<Book, Integer> listWithGenreAssignment;
+	
+	/**ListBooksWithGenreAssignment constructor.
+	 * <br>Calls ListBooks constructor then 
+	 * initialises class variable 'listWithGenreAssignment' as an empty HashMap.
+	 */
+	public ListBooksWithGenreAssignment() {
+		super();
+		this.listWithGenreAssignment = new HashMap<Book, Integer>();
+	}
+
+	public HashMap<Book, Integer> getListWithGenreAssignment() {
+		return listWithGenreAssignment;
+	}
+
+	public void setListWithGenreAssignment(HashMap<Book, Integer> listWithGenreAssignment) {
+		this.listWithGenreAssignment = listWithGenreAssignment;
+	}
+
+	/** Adds an book to class variable 'listWithGenreAssignment'
+	 * @param book Book to be added to the list
+	 * @param i number of users who assigned this book to this genre
+	 */
+	public void addWithGenreAssignment(Book book, Integer i){
+		this.listWithGenreAssignment.put(book, i);
+	}
+	
+	/** This returns a print-friendly String of all the books associated to this genre,
+	 * each toString() and on its own line, with the number of users who assigned it to the genre in parenthesis. 
+	 */
+	@Override
+	public String toString() {
+		String printableList = "";
+		for (Book bo : listWithGenreAssignment.keySet()) {
+			printableList = printableList.concat(bo + " (" + listWithGenreAssignment.get(bo) + ")" + "\n");
+		}
+		return printableList;
+	}	
+
+}
